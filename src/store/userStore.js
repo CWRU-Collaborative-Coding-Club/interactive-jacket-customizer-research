9b2556a1d4f15a37417dea4bc7d307cd17b4841d
alicel22 <all157@case.edu>
2025-04-04 17:15:56 -0400

    added temporal for zundo to state mgmt store

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,34 +1,37 @@
 import { create } from 'zustand'
+import { temporal } from 'zundo';
 
-const userStore = create((set) => ({
-    participant: {
-        id: null,
-        itemID: null, // This will store the design ID of the participant
-    },
-    sessionState: 'inactive',
-    interactionData: [],
+const userStore = create(
+    temporal((set, get) => ({
+        participant: {
+            id: null,
+            itemID: null,
+        },
+        sessionState: 'inactive',
+        interactionData: [],
 
-    setParticipant: (participant) => set({ participant }),
-    setSessionState: (sessionState) => set({ sessionState }),
-    addInteractionData: (interaction) =>
-        set((state) => ({
-            interactionData: [...state.interactionData, interaction],
-        })),
-    clearInteractionData: () =>
-        set(() => ({
-            interactionData: [],
-        })),
-
-        exportData: () => {
-            const { interactionData } = useUserStore.getState();
-            const dataStr = JSON.stringify(interactionData, null, 2);
-            const blob = new Blob([dataStr], { type: 'application/json' });
-            const url = URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = 'interaction_data.json';
-            link.click();
-            URL.revokeObjectURL(url);
-          },
-}))
+        setParticipant: (participant) => set({ participant }),
+        setSessionState: (sessionState) => set({ sessionState }),
+        addInteractionData: (interaction) =>
+            set((state) => ({
+                interactionData: [...state.interactionData, interaction],
+            })),
+        clearInteractionData: () =>
+            set(() => ({
+                interactionData: [],
+            })),
+        
+            exportData: () => {
+                const { interactionData } = useUserStore.getState();
+                const dataStr = JSON.stringify(interactionData, null, 2);
+                const blob = new Blob([dataStr], { type: 'application/json' });
+                const url = URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'interaction_data.json';
+                link.click();
+                URL.revokeObjectURL(url);
+            },
+    })),
+);
 
