c4f64054ced002cf758ccccbb5d5455cd062fb2f
YieumYoon <junsueddie@gmail.com>
2025-04-04 16:35:14 -0400

    added placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <AppLayout>
-      <ThreeColumnLayout
-        leftSidebar={<LeftSidebar />}
-        mainContent={<JacketCanvas />}
-        rightSidebar={<CustomizationPanel />}
-      />
-    </AppLayout>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
   );
 }
 
