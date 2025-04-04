813c66d99c0285cd49e064299a4fbf5780c0712d
emmaahong <emmahong06@gmail.com>
2025-04-04 16:56:44 -0400

    updated store

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 
-const useStore = create((set) => ({
+const userStore = create((set) => ({
     participant: {
         id: null,
         itemID: null, // This will store the design ID of the participant
@@ -8,4 +8,27 @@ const useStore = create((set) => ({
     sessionState: 'inactive',
     interactionData: [],
 
+    setParticipant: (participant) => set({ participant }),
+    setSessionState: (sessionState) => set({ sessionState }),
+    addInteractionData: (interaction) =>
+        set((state) => ({
+            interactionData: [...state.interactionData, interaction],
+        })),
+    clearInteractionData: () =>
+        set(() => ({
+            interactionData: [],
+        })),
+
+        exportData: () => {
+            const { interactionData } = useUserStore.getState();
+            const dataStr = JSON.stringify(interactionData, null, 2);
+            const blob = new Blob([dataStr], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'interaction_data.json';
+            link.click();
+            URL.revokeObjectURL(url);
+          },
+}))
 
